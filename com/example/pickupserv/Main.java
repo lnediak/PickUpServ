5250eb47f7d41aed2b6f5210706d0bc62eb29ef2
Leonid Nediak <lnediak@gmail.com>
2020-02-15 20:57:07 -0500

    wrote Main.java

--- com/example/pickupserv/Main.java
+++ com/example/pickupserv/Main.java
@@ -1,14 +1,9 @@
 package com.example.pickupserv;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Throwable {
+        Server.listen();
     }
-
-    
 
 }
